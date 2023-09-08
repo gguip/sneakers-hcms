749414e41e16bec5f8e0b183fa0dc51c7db67515
Guilherme Passarinho <guilherme.passarinho@goomer.com.br>
2023-09-08 04:56:07 -0300

    disable ssl

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -35,7 +35,7 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: false,
+        ssl: env('DATABASE_SSL', false),
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },

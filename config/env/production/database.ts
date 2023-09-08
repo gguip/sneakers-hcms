623794179437dd0029821466a7670dea2d38e0c5
Guilherme Passarinho <guilherme.passarinho@goomer.com.br>
2023-09-08 05:09:26 -0300

    disable ssl

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -35,7 +35,9 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env('DATABASE_SSL', false),
+        ssl: env('DATABASE_SSL', false) && {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+        },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
